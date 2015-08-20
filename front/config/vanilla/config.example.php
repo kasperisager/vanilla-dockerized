a57c740a757f86e824409b476218bb6eb31eb13f
Kasper Isager <kasperisager@gmail.com>
2015-08-20 14:26:02 +0200

    Add example database config.

--- front/config/vanilla/config.example.php
+++ front/config/vanilla/config.example.php
@@ -4,5 +4,11 @@
 $Configuration['Cache']['Enabled'] = TRUE;
 $Configuration['Cache']['Method'] = 'Memcached';
 
+// Database
+$Configuration['Database']['Name'] = 'vanilla';
+$Configuration['Database']['Host'] = 'database';
+$Configuration['Database']['User'] = 'vanilla';
+$Configuration['Database']['Password'] = 'password';
+
 // Memcached
 $Configuration['Memcached']['Store'] = 'cache:11211';
